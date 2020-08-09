ede47dc34a04ba46a4c3d2d64593f58365b8b516
Shane Chintatkun <schintatkun@gmail.com>
2020-08-09 00:50:13 -0700

    completed app.js | dynamically change caption and images

---
+++ app.js
@@ -0,0 +1,25 @@
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".gallery img");
+const original = document.querySelector(".full-img");
+const caption = document.querySelector(".caption");
+
+previews.forEach((preview) => {
+  preview.addEventListener("click", () => {
+    modal.classList.add("open");
+    original.classList.add("open");
+
+    //Dynamic change caption and image
+    const originalSrc = preview.getAttribute("data-original");
+    original.src = `./img/${originalSrc}`;
+    const altText = preview.alt;
+    caption.textContent = altText;
+  });
+});
+
+modal.addEventListener("click", (e) => {
+  //   console.log(e);
+  if (e.target.classList.contains("modal")) {
+    modal.classList.remove("open");
+    original.classList.remove("open");
+  }
+});
